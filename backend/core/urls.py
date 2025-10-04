caa35ffb754f444d756528851bb48e069e98aa9c
angelo-francisco <ics20080729@gmail.com>
2025-10-04 23:33:29 +0100

    frontend path better set

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-
 from ninja import NinjaAPI
+from courses.api import router as courses_router
 
-api = NinjaAPI()
+api = NinjaAPI(
+    title="Bookar API",
+    csrf=False,
+)
+
+api.add_router('/courses/', courses_router)
 
 urlpatterns = [
     path('api/', api.urls),
