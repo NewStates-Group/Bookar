f681dda44e119699cdd51a6bbc80baef25ed22e4
angelo-francisco <ics20080729@gmail.com>
2025-10-24 20:20:38 +0100

    feature: video, module and course models

--- backend/courses/models.py
+++ backend/courses/models.py
@@ -1,8 +1,11 @@
+from decimal import DefaultContext
+from os import name
 from django.db import models
 
 
 class Tag(models.Model):
     name = models.CharField(max_lenght=80, null=True, blank=True)
+
 
 class Course(models.Model):
     thumb = models.ImageField(upload_to="courses/thumbs/", null=True, blank=True)
@@ -15,5 +18,23 @@ class Course(models.Model):
     title = models.CharField(max_lenght=80, null=True, blank=True)
     desc = models.TextField(null=True, blank=True)
     duration = models.CharField(max_lenght=15, null=True, blank=True)
-    tags = models.ManyToManyField(Tag)
-    # TERMINARS
+    tag = models.ForeignKey(Tag)
+    level = models.CharField(
+        max_lenght=1,
+        choices=[("I", "Iniciante"), ("IT", "Intermediário"), ("A", "Avançado")],
+    )
+    modules = models.ManyToManyField("Module")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Module(models.Model):
+    name = models.CharField(max_lenght=80, null=True, blank=True)  # NOQA
+    videos = models.ManyToManyField("Video")
+
+
+class Video(models.Model):
+    title = models.CharField(max_lenght=80, null=True, blank=True)
+    file = models.FileField(upload_to="courses/videos/", null=True, blank=True)
+    duraction = models.PositiveIntegerField(default=0, null=True, blank=True)
+    watched = models.BooleanField(default=False, null=True, blank=True)
+    desc = models.TextField(null=True, blank=True)
