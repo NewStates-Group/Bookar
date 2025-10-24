799977566ac8dbf684042f3b5557ab3919f2f943
angelo-francisco <ics20080729@gmail.com>
2025-10-24 20:43:38 +0100

    fix bugs and new dep: Pillow

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -13,49 +13,49 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a7ht-+08f*@^n7%xe=9fm*u+1ga-+!kc#bdt%+ir(k^-qc1eyg'
+SECRET_KEY = "django-insecure-a7ht-+08f*@^n7%xe=9fm*u+1ga-+!kc#bdt%+ir(k^-qc1eyg"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
-    'ninja',
-    'corsheaders',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'courses'
+    "ninja",
+    "corsheaders",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "courses",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.common.CommonMiddleware',
+    "django.middleware.common.CommonMiddleware",
 ]
 
-ROOT_URLCONF = 'core.urls'
+ROOT_URLCONF = "core.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'core.wsgi.application'
+WSGI_APPLICATION = "core.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {"default": env.db()}
+DATABASES = {"default": env.db_url_config(os.getenv("DATABASE_URL"))}
 
 
 # Password validation
@@ -63,16 +63,16 @@ DATABASES = {"default": env.db()}
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -80,9 +80,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'pt-br'
+LANGUAGE_CODE = "pt-br"
 
-TIME_ZONE = 'Africa/Luanda'
+TIME_ZONE = "Africa/Luanda"
 
 USE_I18N = True
 
@@ -90,10 +90,10 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR.joinpath('media')
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR.joinpath("media")
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_ALLOW_ALL = True
--- backend/courses/models.py
+++ backend/courses/models.py
@@ -1,39 +1,33 @@
-from decimal import DefaultContext
-from os import name
 from django.db import models
 
 
 class Tag(models.Model):
-    name = models.CharField(max_lenght=80, null=True, blank=True)
+    name = models.CharField(max_length=80, null=True, blank=True)
 
 
 class Course(models.Model):
     thumb = models.ImageField(upload_to="courses/thumbs/", null=True, blank=True)
     level = models.CharField(
-        max_lenght=1,
+        max_length=2,
         choices=[("I", "Iniciante"), ("IT", "Intermediário"), ("A", "Avançado")],
         null=True,
         blank=True,
     )
-    title = models.CharField(max_lenght=80, null=True, blank=True)
+    title = models.CharField(max_length=80, null=True, blank=True)
     desc = models.TextField(null=True, blank=True)
-    duration = models.CharField(max_lenght=15, null=True, blank=True)
-    tag = models.ForeignKey(Tag)
-    level = models.CharField(
-        max_lenght=1,
-        choices=[("I", "Iniciante"), ("IT", "Intermediário"), ("A", "Avançado")],
-    )
+    duration = models.CharField(max_length=10, null=True, blank=True)
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
     modules = models.ManyToManyField("Module")
     created_at = models.DateTimeField(auto_now_add=True)
 
 
 class Module(models.Model):
-    name = models.CharField(max_lenght=80, null=True, blank=True)  # NOQA
+    name = models.CharField(max_length=80, null=True, blank=True)  # NOQA
     videos = models.ManyToManyField("Video")
 
 
 class Video(models.Model):
-    title = models.CharField(max_lenght=80, null=True, blank=True)
+    title = models.CharField(max_length=80, null=True, blank=True)
     file = models.FileField(upload_to="courses/videos/", null=True, blank=True)
     duraction = models.PositiveIntegerField(default=0, null=True, blank=True)
     watched = models.BooleanField(default=False, null=True, blank=True)
