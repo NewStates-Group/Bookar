7be8673ebeec4007a33867846bea90f4dce68845
angelo-francisco <ics20080729@gmail.com>
2026-01-15 20:36:35 +0100

    adiciona endpoint para obter dados do user

--- backend/accounts/controllers.py
+++ backend/accounts/controllers.py
@@ -1,5 +1,6 @@
 from injector import inject
 from ninja_extra import api_controller, route
+from ninja_jwt.authentication import JWTAuth
 from ninja_jwt.controller import NinjaJWTDefaultController
 
 from .schemas import RegisterIn, RegisterOut
@@ -15,3 +16,7 @@ class AuthController(NinjaJWTDefaultController):
     @route.post("signup", response=RegisterOut)
     def signup(self, data: RegisterIn):
         return self.auth_service.create_user(**data.dict())
+
+    @route.get("me", response=RegisterOut, auth=JWTAuth())
+    def me(self, request):
+        return request.user
