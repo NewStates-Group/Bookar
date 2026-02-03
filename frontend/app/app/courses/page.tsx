19a9fa961d99582afff00ed65b95cf9ce83941ef
angelo-francisco <ics20080729@gmail.com>
2026-02-04 00:30:37 +0100

    fix card padding

--- frontend/app/app/courses/page.tsx
+++ frontend/app/app/courses/page.tsx
@@ -302,7 +302,7 @@ export default function CoursesPage() {
           </p>
         </div>
       ) : (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
           {courses.map((course) => (
             <Card
               key={`${course.id}`}
