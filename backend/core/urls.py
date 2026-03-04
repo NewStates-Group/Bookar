f9f41ab6045fa14e0ad98477145fb4c35263cafb
angelo-francisco <ics20080729@gmail.com>
2026-03-04 21:23:13 +0100

    healthcheck added

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,9 +1,16 @@
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import JsonResponse
 from django.urls import path
 
 from .api import api
 
+
+def healthcheck(request):
+    return JsonResponse({"status": "ok"})
+
+
 urlpatterns = [
+    path("healthcheck/", healthcheck),
     path("api/", api.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
