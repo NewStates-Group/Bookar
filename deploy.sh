e87aee4ce8725a687688d36a3b52c936052eccf3
angelo-francisco <ics20080729@gmail.com>
2026-03-09 00:03:15 +0100

    deploy.sh updated

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,9 @@ set -e
 
 # echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
 
-docker compose pull
+docker compose -f docker-compose.prod.yml pull
 
-docker compose up -d --build --remove-orphans
+docker compose -f docker-compose.prod.yml up -d --build --remove-orphans
 
 docker image prune -f
 
