48a7b5e3b3f8ccd71d93054424950484aa984b9a
angelo-francisco <ics20080729@gmail.com>
2026-03-09 01:33:30 +0100

    organizing,better deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -4,11 +4,11 @@ set -e
 # Logar no Docker (se precisar)
 # echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
 
-docker compose -f docker-compose.prod.yml pull
+docker compose pull
 
-docker compose -f docker-compose.prod.yml down
+docker compose down
 
-docker compose -f docker-compose.prod.yml up -d --build --remove-orphans
+docker compose up -d --build --remove-orphans
 
 docker image prune -f
 
