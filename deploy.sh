75d3785d80d39c5cc64562390b73ecb6f50220df
angelo-francisco <ics20080729@gmail.com>
2026-03-09 01:09:39 +0100

    new updates

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-
 set -e
 
+# Logar no Docker (se precisar)
 # echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
 
 docker compose -f docker-compose.prod.yml pull
+
+docker compose -f docker-compose.prod.yml down
 
 docker compose -f docker-compose.prod.yml up -d --build --remove-orphans
 
