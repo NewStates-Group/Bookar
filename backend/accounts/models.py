1dbc2472b5de6282284f8f16c9f03d5d7048da88
angelo-francisco <ics20080729@gmail.com>
2026-03-11 19:50:16 +0100

    Backend: Add EmailVerificationCode model and migrations

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -10,3 +10,12 @@ class User(AbstractUser):
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
+
+
+class EmailVerificationCode(models.Model):
+    email = models.EmailField()
+    code = models.CharField(max_length=6)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.email} - {self.code}"
