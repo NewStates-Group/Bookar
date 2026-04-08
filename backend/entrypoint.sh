8fd5bb3249b11dcddefe306e4cafcaf4ded21ce8
angelo-francisco <ics20080729@gmail.com>
2026-04-08 10:38:13 +0100

    worker can be executed via bookar-backend

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,11 +1,24 @@
 #!/bin/sh
 
+echo "Applying migrations"
 python manage.py migrate --noinput
 
-if [ "$DEBUG" = "True" ] || [ "$DEBUG" = "1" ]; then
-    echo "Starting Gunicorn with --reload"
-    exec gunicorn core.asgi:application --workers 2 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000 --reload
-else
-    exec gunicorn core.asgi:application --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000
+if [ "$WORKER" = "True" ]; then
+    echo "Starting Celery worker"
+    exec celery -A core worker --loglevel=info
 fi
 
+echo "Starting Gunicorn (DEBUG=$DEBUG)"
+
+if [ "$DEBUG" = "True" ] || [ "$DEBUG" = "1" ]; then
+    exec gunicorn core.asgi:application \
+        --workers 2 \
+        --worker-class uvicorn.workers.UvicornWorker \
+        --bind 0.0.0.0:8000 \
+        --reload
+else
+    exec gunicorn core.asgi:application \
+        --workers 4 \
+        --worker-class uvicorn.workers.UvicornWorker \
+        --bind 0.0.0.0:8000
+fi
